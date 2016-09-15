2a92d19d8e86bc2f032d1779e41ff898b5cce6ec
diminator <dimi.dejonghe@gmail.com>
2016-09-15 12:17:39 +0200

    docs(exercises): hashlock instructions

---
+++ bigchaindb-workshop/exercises/cryptoconditions/ex2_hashlock.py
@@ -0,0 +1,5 @@
+# 1. Find a good secret and hash it (eg. 'answer to life the universe and everything')
+
+# 2. Hashlocks
+# 2.a Create a Hashlock fulfillment and broadcast the condition URI
+# 2.b Validate the fulfillment
