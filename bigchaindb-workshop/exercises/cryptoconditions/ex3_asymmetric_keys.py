9489e33c7dae6b7666d7c92f291c8873a645c144
diminator <dimi.dejonghe@gmail.com>
2016-09-15 12:53:57 +0200

    feat(exercises): asymmetric keys

---
+++ bigchaindb-workshop/exercises/cryptoconditions/ex3_asymmetric_keys.py
@@ -0,0 +1,10 @@
+# 1. Generate a random key pair
+# 1.a Encode the key pairs to bytes, hex, base58 and base64
+
+# 2.a Create an ED25519-SHA256 Condition
+# 2.b Fulfill an ED25519-SHA256 condition
+
+# 3 Fulfill an ED25519-SHA256 Fulfillment URI
+example_fulfillment_uri = \
+    'cf:4:7Bcrk61eVjv0kyxw4SRQNMNUZ-8u_U1k6_gZaDRn4r-2IpH62UMvjymL' \
+    'nEpIldvik_b_2hpo2t8Mze9fR6DHISpf6jzal6P0wD6p8uisHOyGpR1FISer26CdG28zHAcK'
